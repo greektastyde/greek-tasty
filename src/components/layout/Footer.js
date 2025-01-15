dd6e1f4fa7c788d0b1b8a82b3a0552e906ed6271
greektastyde <greektastyde@gmail.com>
2025-01-15 15:21:27 +0100

    Update Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,9 +4,9 @@ export default function Footer() {
         <footer>
             <div className=" p-2 border-t  mt-32">
                 <div className=" mx-auto flex flex-col text-center py-1 px-4 md:flex-row  md:justify-between md:px-8 inria">
-                    &copy; 2025 All rights reserved
+                    &copy; 2025 Alle Rechte vorbehalten
                     <div className=" text-center gap-5 inria ">
-                        <a href="/policy">Privacy policy</a>
+                        <a href="/policy">Datenschutzrichtlinie</a>
                     </div>
                 </div>
             </div>
